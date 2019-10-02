60e95064d4e269eefbb62c4762fe961715a04c41
Chandrika Mudduluru <40604431+cmudduluru@users.noreply.github.com>
2019-10-02 13:43:31 -0500

    Added Speciality Map Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/com/learnspring/sfpetclinic/services/SpecialityService.java
@@ -0,0 +1,7 @@
+package com.learnspring.sfpetclinic.services;
+
+import com.learnspring.sfpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
+
