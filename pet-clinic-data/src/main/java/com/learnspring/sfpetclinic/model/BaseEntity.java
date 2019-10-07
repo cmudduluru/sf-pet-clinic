9270cbe88eb5928a6dbdfca9cd6353180f886089
Chandrika Mudduluru <40604431+cmudduluru@users.noreply.github.com>
2019-10-07 15:41:16 -0500

    updated base entity with JPA Annotations. Closes #35

--- pet-clinic-data/src/main/java/com/learnspring/sfpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/learnspring/sfpetclinic/model/BaseEntity.java
@@ -1,8 +1,17 @@
 package com.learnspring.sfpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     public Long getId() {
         return id;
@@ -11,6 +20,4 @@ public class BaseEntity implements Serializable {
     public void setId(Long id) {
         this.id = id;
     }
-
-    private Long id;
 }
